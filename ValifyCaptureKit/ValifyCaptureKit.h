c65bb68add4b61eff464bd5bab542b0d7e7909b6
sala7khaled <sala7khaled7@gmail.com>
2024-10-13 20:53:09 +0300

    Initial Commit

---
+++ ValifyCaptureKit/ValifyCaptureKit.h
@@ -0,0 +1,18 @@
+//
+//  ValifyCaptureKit.h
+//  ValifyCaptureKit
+//
+//  Created by Salah Khaled on 13/10/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ValifyCaptureKit.
+FOUNDATION_EXPORT double ValifyCaptureKitVersionNumber;
+
+//! Project version string for ValifyCaptureKit.
+FOUNDATION_EXPORT const unsigned char ValifyCaptureKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValifyCaptureKit/PublicHeader.h>
+
+
