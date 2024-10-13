487431c53412fe2f3c2772841241160ad392ba74
sala7khaled <sala7khaled7@gmail.com>
2024-10-13 20:59:22 +0300

    first init

---
+++ ValifyCaptureKit/CaptureController/CaptureController.swift
@@ -0,0 +1,29 @@
+//
+//  CaptureController.swift
+//  ValifyCaptureKit
+//
+//  Created by Salah Khaled on 13/10/2024.
+//
+
+import UIKit
+
+class CaptureController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
